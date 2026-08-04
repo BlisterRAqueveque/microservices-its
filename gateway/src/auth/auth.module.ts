2584c30794a7f2223c356093ba4d1d9286b498dc
Roberto Aqueveque <141036948+BlisterRAqueveque@users.noreply.github.com>
2026-08-04 17:24:53 -0300

    Fix: dependencies

--- gateway/src/auth/auth.module.ts
+++ gateway/src/auth/auth.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { envs } from 'src/config';
+import { envs, USER_MS } from 'src/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    ClientsModule.register([
+      {
+        name: USER_MS,
+        transport: Transport.TCP,
+        options: { port: envs.USER_MS_PORT, host: envs.USER_MS_HOST },
+      },
+    ]),
     JwtModule.register({
       secret: envs.SEED,
       signOptions: { expiresIn: '8h' },
